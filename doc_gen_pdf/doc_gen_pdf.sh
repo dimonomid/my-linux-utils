0c5783d0d5c67ac8c2a6ad3551db5f1e3728c128
Dmitry Frank <mail@dmitryfrank.com>
2017-04-08 16:56:37 +0300

    Add doc_gen_pdf
    An utility to generate pdf files from txt Linux Kernel Documentation

---
+++ doc_gen_pdf/doc_gen_pdf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SRC="$1"
+DST="$2"
+
+# --no-header
+ENSCRIPT_FLAGS="--font 'CourierNew-Bold11' --margins=10:10:10:80"
+
+if [[ "x${SRC}" == "x" ]]; then
+  echo "Generate pdf output from txt Linux Kernel Documentation"
+  echo "Usage: $0 /path/to/linux-source/Documentation [/path/to/output-dir]"
+  exit 1
+fi
+
+if [[ "x${DST}" == "x" ]]; then
+  DST="./output"
+fi
+
+DST="$(readlink -f ${DST})"
+
+mkdir -p "${DST}"
+
+pushd "${SRC}"
+
+find -name "*.txt" -exec bash -c " \
+  echo -n 'Converting {}... ' && \
+  mkdir -p \"${DST}/\$(dirname {})\" && \
+  enscript ${ENSCRIPT_FLAGS} -p - {} | ps2pdf - '${DST}/{}.pdf'" \;
+
+popd
